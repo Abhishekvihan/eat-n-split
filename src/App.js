1f009fae5569dc4699f25a243b55ff0b4ce87846
abhishekvihan2 <abhishekvihan2@gmail.com>
2024-04-08 11:46:28 +0530

    static comp

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 const initialFriends = [
   {
     id: 118836,
@@ -18,3 +19,47 @@ const initialFriends = [
     balance: 0,
   },
 ];
+
+export default function App() {
+  return (
+    <div className="app">
+      <div className="sidebar"></div>
+      <FriendList />
+    </div>
+  );
+}
+
+function FriendList() {
+  const friends = initialFriends;
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+
+      {friend.balance < 0 && (
+        <p className="red">
+          {" "}
+          You owe {friend.name} {Math.abs(friend.balance)}$
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {" "}
+          You owe {friend.name} {Math.abs(friend.balance)}$
+        </p>
+      )}
+      {friend.balance === 0 && <p> You owe {friend.name} are even</p>}
+      <button className="button">Select</button>
+    </li>
+  );
+}
